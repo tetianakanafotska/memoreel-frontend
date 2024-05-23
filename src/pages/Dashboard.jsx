73c0870638336c1f6617f220b89887fb1c316dda
tanyakanf <tanyakanf@gmail.com>
2024-05-24 00:43:05 +0200

    fix current date & iso method

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,8 +13,18 @@ const Dashboard = () => {
   const [mediaType, setMediaType] = useState(null);
   const [allMedia, setAllMedia] = useState([]);
 
+  const getLocalISODate = () => {
+    const now = new Date();
+    const offset = now.getTimezoneOffset() * 60000;
+    const localISOTime = new Date(now.getTime() - offset)
+      .toISOString()
+      .slice(0, 10);
+    return localISOTime;
+  };
+
   useEffect(() => {
-    const currentDate = new Date().toISOString().slice(0, 10);
+    const currentDate = getLocalISODate();
+    console.log("THE DATE", currentDate);
     if (user) {
       const userId = user._id;
       axios
