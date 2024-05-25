c9b97a0789957c2b69b6d344184045f2f4ffdfa6
Laura Sinclair <l.sinclair@tuta.io>
2024-05-25 16:07:39 +0200

    User profile pic

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,87 +1,97 @@
-import React, { useState, useContext, useEffect } from "react";
-import PopUpButtons from "../components/PopUpButtons";
-import MediaForm from "../components/MediaForm";
-import MediaItem from "../components/MediaItem";
-import { AuthContext } from "@context";
-import axios from "axios";
+import React, { useState, useContext, useEffect } from 'react';
+import PopUpButtons from '../components/PopUpButtons';
+import MediaForm from '../components/MediaForm';
+import MediaItem from '../components/MediaItem';
+import { AuthContext } from '@context';
+import axios from 'axios';
+import placeholder from '@img/placeholder.jpg';
 
 const Dashboard = () => {
-  const { user } = useContext(AuthContext);
-  const [boardId, setBoardId] = useState(null);
-  const [openPopUp, setOpenPopUp] = useState(false);
-  const [openMediaForm, setOpenMediaForm] = useState(false);
-  const [mediaType, setMediaType] = useState(null);
-  const [allMedia, setAllMedia] = useState([]);
+	const { user } = useContext(AuthContext);
+	const [boardId, setBoardId] = useState(null);
+	const [openPopUp, setOpenPopUp] = useState(false);
+	const [openMediaForm, setOpenMediaForm] = useState(false);
+	const [mediaType, setMediaType] = useState(null);
+	const [allMedia, setAllMedia] = useState([]);
 
-  useEffect(() => {
-    const currentDate = new Date().toISOString().slice(0, 10);
-    console.log("THE DATE", currentDate);
-    if (user) {
-      const userId = user._id;
-      axios
-        .get(
-          `http://localhost:5005/users/${userId}/boards?start=${currentDate}`
-        )
-        .then((res) => {
-          setAllMedia(res.data[0].boardContent);
-          setBoardId(res.data[0]._id);
-        });
-    }
-  }, [user]);
+	useEffect(() => {
+		const currentDate = new Date().toISOString().slice(0, 10);
+		console.log('THE DATE', currentDate);
+		if (user) {
+			const userId = user._id;
+			axios
+				.get(
+					`http://localhost:5005/users/${userId}/boards?start=${currentDate}`
+				)
+				.then((res) => {
+					setAllMedia(res.data[0].boardContent);
+					setBoardId(res.data[0]._id);
+				});
+		}
+	}, [user]);
 
-  useEffect(() => {
-    if (!boardId && user) {
-      axios
-        .post(`http://localhost:5005/boards`, {
-          userId: user._id,
-          boardContent: allMedia,
-        })
-        .then((res) => {
-          console.log(res);
-        });
-    }
-    if (boardId && user) {
-      axios
-        .patch(`http://localhost:5005/boards/${boardId}`, allMedia)
-        .then((res) => {
-          console.log(res);
-        });
-    }
-  }, [allMedia]);
+	useEffect(() => {
+		if (!boardId && user) {
+			axios
+				.post(`http://localhost:5005/boards`, {
+					userId: user._id,
+					boardContent: allMedia,
+				})
+				.then((res) => {
+					console.log(res);
+				});
+		}
+		if (boardId && user) {
+			axios
+				.patch(`http://localhost:5005/boards/${boardId}`, allMedia)
+				.then((res) => {
+					console.log(res);
+				});
+		}
+	}, [allMedia]);
 
-  return (
-    <>
-      <h2>{user ? `Hello, ${user.name}!` : "Hello!"}</h2>
+	return (
+		<>
+			<h2>{user ? `Hello, ${user.name}!` : 'Hello!'}</h2>
 
-      <div className="dashboard-container">
-        <button onClick={() => setOpenPopUp(!openPopUp)}>+</button>
-        {openPopUp && (
-          <PopUpButtons
-            setMediaType={setMediaType}
-            setOpenMediaForm={setOpenMediaForm}
-            setOpenPopUp={setOpenPopUp}
-          />
-        )}
-        {openMediaForm && (
-          <MediaForm
-            mediaType={mediaType}
-            setAllMedia={setAllMedia}
-            setOpenPopUp={setOpenPopUp}
-            setOpenMediaForm={setOpenMediaForm}
-          />
-        )}
-        {allMedia &&
-          allMedia.map((media, index) => {
-            //replace with decent key - media._id
-            return (
-              <div key={index}>
-                <MediaItem media={media} />
-              </div>
-            );
-          })}
-      </div>
-    </>
-  );
+			{user && (
+				<div className='profilePic'>
+					<img
+						src={user.profileImg}
+						alt={user.name}
+					/>
+				</div>
+			)}
+
+			<div className='dashboard-container'>
+				<button onClick={() => setOpenPopUp(!openPopUp)}>+</button>
+				{openPopUp && (
+					<PopUpButtons
+						setMediaType={setMediaType}
+						setOpenMediaForm={setOpenMediaForm}
+						setOpenPopUp={setOpenPopUp}
+					/>
+				)}
+				{openMediaForm && (
+					<MediaForm
+						mediaType={mediaType}
+						setAllMedia={setAllMedia}
+						setOpenPopUp={setOpenPopUp}
+						setOpenMediaForm={setOpenMediaForm}
+					/>
+				)}
+				{allMedia &&
+					allMedia.map((media, index) => {
+						//replace with decent key - media._id
+						return (
+							<div key={index}>
+								<MediaItem media={media} />
+							</div>
+						);
+					})}
+			</div>
+		</>
+	);
 };
 
 export default Dashboard;
