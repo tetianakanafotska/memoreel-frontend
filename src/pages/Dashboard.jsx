c34aee576375bdf17536c5d3c5933377bfbf71be
tanyakanf <tanyakanf@gmail.com>
2024-05-26 15:32:28 +0200

    add assets route and implement edit/delete asset functionality

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,57 +4,70 @@ import MediaForm from "../components/MediaForm";
 import MediaItem from "../components/MediaItem";
 import { AuthContext } from "@context";
 import axios from "axios";
-//import placeholder from "@img/placeholder.jpg";
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
   const [boardId, setBoardId] = useState(null);
   const [openPopUp, setOpenPopUp] = useState(false);
   const [openMediaForm, setOpenMediaForm] = useState(false);
-  const [mediaType, setMediaType] = useState(null);
-  const [allMedia, setAllMedia] = useState([]);
+  const [assetType, setAssetType] = useState(null);
+  const [allAssets, setAllAssets] = useState([]);
+
+  const handleDeleteAsset = (assetId) => {
+    console.log("asset to be deleted", assetId);
+    axios
+      .delete(`http://localhost:5005/assets/${assetId}`)
+      .then((res) => {
+        console.log("resp from deleting", res);
+        setAllAssets((prevAssets) =>
+          prevAssets.filter((asset) => asset._id !== assetId)
+        );
+      })
+      .catch((err) => {
+        console.error("Error deleting asset", err);
+      });
+  };
+
+  const handleEditAsset = (assetId, editedContent) => {
+    console.log("editedContent", editedContent);
+    axios
+      .put(`http://localhost:5005/assets/${assetId}`, {
+        content: editedContent,
+      })
+      .then((res) => {
+        const updatedAsset = res.data;
+        console.log("updated res", res);
+        setAllAssets((prevAssets) =>
+          prevAssets.map((asset) =>
+            asset._id === assetId ? updatedAsset : asset
+          )
+        );
+      })
+      .catch((err) => {
+        console.error("Error updating asset", err);
+      });
+  };
 
   useEffect(() => {
     const currentDate = new Date().toISOString().slice(0, 10);
     console.log("THE DATE", currentDate);
     if (user) {
-      const userId = user._id;
       axios
         .get(
-          `http://localhost:5005/users/${userId}/boards?start=${currentDate}`
+          `http://localhost:5005/users/${user._id}/boards?start=${currentDate}`
         )
         .then((res) => {
           if (res.data.length !== 0) {
-            setAllMedia(res.data[0].boardContent);
+            setAllAssets(res.data[0].assets);
             setBoardId(res.data[0]._id);
+            console.log("Existing board found. BoardID:", res.data[0]._id);
           } else {
-            setAllMedia([]);
+            setAllAssets([]);
             setBoardId(null);
           }
         });
     }
   }, [user]);
-
-  useEffect(() => {
-    console.log("this is board id:", boardId);
-    if (boardId === null && allMedia.length > 0 && user) {
-      axios
-        .post(`http://localhost:5005/boards`, {
-          userId: user._id,
-          boardContent: allMedia,
-        })
-        .then((res) => {
-          console.log(res);
-          setBoardId(res.data._id);
-        });
-    } else if (boardId && user) {
-      axios
-        .patch(`http://localhost:5005/boards/${boardId}`, allMedia)
-        .then((res) => {
-          console.log(res);
-        });
-    }
-  }, [allMedia, boardId, user]);
 
   return (
     <>
@@ -68,23 +81,29 @@ const Dashboard = () => {
         <button onClick={() => setOpenPopUp(!openPopUp)}>+</button>
         {openPopUp && (
           <PopUpButtons
-            setMediaType={setMediaType}
+            setAssetType={setAssetType}
             setOpenMediaForm={setOpenMediaForm}
             setOpenPopUp={setOpenPopUp}
           />
         )}
         {openMediaForm && (
           <MediaForm
-            mediaType={mediaType}
-            setAllMedia={setAllMedia}
+            assetType={assetType}
             setOpenPopUp={setOpenPopUp}
             setOpenMediaForm={setOpenMediaForm}
+            setAllAssets={setAllAssets}
+            boardId={boardId}
+            userId={user._id}
           />
         )}
-        {allMedia.length > 0 ? (
-          allMedia.map((media, index) => (
-            <div key={index}>
-              <MediaItem media={media} />
+        {allAssets.length > 0 ? (
+          allAssets.map((asset) => (
+            <div key={asset._id}>
+              <MediaItem
+                asset={asset}
+                handleDeleteAsset={handleDeleteAsset}
+                handleEditAsset={handleEditAsset}
+              />
             </div>
           ))
         ) : (
