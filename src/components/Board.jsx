f9abb27e4ff7d67c77d099224c9dacdcd92b0317
Laura Sinclair <l.sinclair@tuta.io>
2024-05-26 17:52:30 +0200

    Quick css refactor

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,39 +4,60 @@ import classNames from 'classnames';
 
 function Board({ board }) {
 	const { id, createdAt } = board;
-  
+
+	const renderContent = (key, value) => {
+		if (!value) return null;
+		switch (value.type) {
+			case 'image':
+				return (
+					<div
+						className={classNames(
+							styles.boardImage,
+							styles.boardElement
+						)}>
+						<img
+							src={value.content}
+							alt={key}
+						/>
+					</div>
+				);
+			case 'text':
+				return (
+					<div
+						className={classNames(
+							styles.boardNote,
+							styles.boardElement
+						)}>
+						<div>
+							<p>{value.content}</p>
+						</div>
+					</div>
+				);
+			case 'videoURL':
+				return (
+					<div
+						className={classNames(
+							styles.boardNote,
+							styles.boardElement
+						)}>
+						<div>
+							<p>Still to implement {value.content}</p>
+						</div>
+					</div>
+				);
+
+			default:
+				return null;
+		}
+	};
+
 	return (
 		<div className={styles.board}>
 			{board &&
 				board.boardContent &&
 				Object.entries(board.boardContent).map(([key, value]) => {
 					return (
-						<>
-							<div key={id}>
-								{value.type === 'image' && (
-									<div className={classNames(styles.boardImage, styles.boardElement)}>
-										<img
-											src={value.content}
-											alt={key}
-										/>
-									</div>
-								)}
-								{value.type === 'text' && (
-									<div className={classNames(styles.boardNote, styles.boardElement)}>
-										<div>
-											<p>{value.content}</p>
-										</div>
-									</div>
-								)}
-								{value.type === 'video' && (
-									<div className={classNames(styles.boardNote, styles.boardElement)}>
-										<div>
-											<p>{value.content}</p>
-										</div>
-									</div>
-								)}
-							</div>
-						</>
+						<>{<div key={id}>{renderContent(key, value)}</div>}</>
 					);
 				})}
 		</div>
