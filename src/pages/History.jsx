0c72e7aaccdce80b1aea0e0dab7ec04f2c7a9f7f
Laura Sinclair <l.sinclair@tuta.io>
2024-05-27 20:58:39 +0200

    History page: boards look nice

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,36 +1,37 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "@context";
-import axios from "axios";
-import authService from "../services/auth.service";
-import { Board } from "@components";
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '@context';
+import axios from 'axios';
+import authService from '../services/auth.service';
+import { Board, MediaItem } from '@components';
+import boardStyles from '../components/styles/Board.module.sass';
 
 function History() {
-  const { user } = useContext(AuthContext);
-  const [allBoards, setAllboards] = useState([]);
-  const token = localStorage.getItem("authToken");
+	const { user } = useContext(AuthContext);
+	const [allBoards, setAllboards] = useState([]);
+	const [allAssets, setAllAssets] = useState([]);
+	const [boardId, setBoardId] = useState(null);
+	const token = localStorage.getItem('authToken');
 
-  useEffect(() => {
-    if (user) {
-      const userId = user._id;
-      const start = "2023-05-01";
-      const end = "2024-12-12";
-
-      axios
-        .get(
-          `${
-            import.meta.env.BACKEND_URL || "http://localhost:5005"
-          }/users/${userId}/boards?start=${start}&end=${end}`,
-          {
-            headers: { Authorization: ` Bearer ${token}` },
-          }
-        )
-        .then((res) => {
-          setAllboards(res.data);
-          console.log(res);
-        })
-        .catch((error) => console.error("Oops", error));
-    }
-  }, [user]);
+	useEffect(() => {
+		if (user) {
+			axios
+				.get(`http://localhost:5005/users/${user._id}/boards`, {
+					headers: { Authorization: ` Bearer ${token}` },
+				})
+				.then((res) => {
+					if (res.data.length !== 0) {
+						setAllboards(res.data);
+						setAllAssets(res.data[0].assets);
+						// setBoardId(res.data[0]._id);
+						// console.log(
+						// 	'Existing board found. BoardID:',
+						// 	res.data[0]._id
+						// );
+					}
+				})
+				.catch((error) => console.log('Error fetching boards' + error));
+		}
+	}, [user]);
 
 	return (
 		<>
@@ -40,12 +41,24 @@ function History() {
 					? 'No board to show yet!'
 					: allBoards.map((board, i) => {
 							return (
-								<div key={board._id}>
-									<h3>Board {i}</h3>
-									<Board board={board} />
-								</div>
+								<>
+									<div key={board._id}>
+										<h3>Board {i}</h3>
+
+										<div className={boardStyles.board}>
+											{allAssets.length > 0 &&
+												allAssets.map((asset) => (
+													<div key={asset._id}>
+														<MediaItem
+															asset={asset}
+														/>
+													</div>
+												))}
+										</div>
+									</div>
+								</>
 							);
-					}))}
+						}))}
 		</>
 	);
 }
