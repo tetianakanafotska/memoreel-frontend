eedda49717f3e300b37f683dd17463479c083087
Laura Sinclair <l.sinclair@tuta.io>
2024-05-27 12:41:21 +0200

    Polaroid style for selfies

--- src/components/MediaItem.jsx
+++ src/components/MediaItem.jsx
@@ -1,84 +1,110 @@
-import React, { useEffect, useState } from "react";
-import ReactPlayer from "react-player/youtube";
+import classNames from 'classnames';
+import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player/youtube';
+import boardStyles from './styles/Board.module.sass';
 
 function MediaItem({ asset, handleDeleteAsset, handleEditAsset }) {
-  const [isEditing, setIsEditing] = useState(false);
-  const [assetContent, setAssetContent] = useState(asset.content);
+	const [isEditing, setIsEditing] = useState(false);
+	const [assetContent, setAssetContent] = useState(asset.content);
 
-  const handleSaveEdit = () => {
-    handleEditAsset(asset._id, assetContent);
-    setIsEditing(false);
-  };
+	const handleSaveEdit = () => {
+		handleEditAsset(asset._id, assetContent);
+		setIsEditing(false);
+	};
 
-  const renderContent = () => {
-    switch (asset.type) {
-      case "text":
-        return isEditing ? (
-          <input
-            type="text"
-            value={assetContent}
-            onChange={(e) => setAssetContent(e.target.value)}
-          />
-        ) : (
-          <p>{assetContent}</p>
-        );
-      case "image":
-        return isEditing ? (
-          <input
-            type="text"
-            value={assetContent}
-            onChange={(e) => setAssetContent(e.target.value)}
-          />
-        ) : (
-          <img src={assetContent} alt="Uploaded content" />
-        );
-      case "youtubeURL":
-        return isEditing ? (
-          <>
-            <input
-              type="text"
-              value={assetContent}
-              onChange={(e) => setAssetContent(e.target.value)}
-            />
+	const renderContent = () => {
+		switch (asset.type) {
+			case 'text':
+				return isEditing ? (
+					<input
+						type='text'
+						value={assetContent}
+						onChange={(e) => setAssetContent(e.target.value)}
+					/>
+				) : (
+					<div
+						className={classNames(
+							boardStyles.boardNote,
+							boardStyles.boardElement
+						)}>
+						<div>
+							<p>{assetContent}</p>
+						</div>
+					</div>
+				);
+			case 'image':
+				return isEditing ? (
+					<input
+						type='text'
+						value={assetContent}
+						onChange={(e) => setAssetContent(e.target.value)}
+					/>
+				) : (
+					<img
+						src={assetContent}
+						alt='Uploaded content'
+					/>
+				);
+			case 'youtubeURL':
+				return isEditing ? (
+					<>
+						<input
+							type='text'
+							value={assetContent}
+							onChange={(e) => setAssetContent(e.target.value)}
+						/>
 
-            <ReactPlayer url={assetContent} controls />
-          </>
-        ) : (
-          <ReactPlayer url={assetContent} controls />
-        );
-      case "camImage":
-        return isEditing ? (
-          <input
-            type="text"
-            value={asset.content}
-            onChange={(e) => setEditedContent(e.target.value)}
-          />
-        ) : (
-          <img src={asset.content} alt="Uploaded content" />
-        );
-      default:
-        return null;
-    }
-  };
+						<ReactPlayer
+							url={assetContent}
+							controls
+						/>
+					</>
+				) : (
+					<ReactPlayer
+						url={assetContent}
+						controls
+					/>
+				);
+			case 'camImage':
+				return isEditing ? (
+					<input
+						type='text'
+						value={asset.content}
+						onChange={(e) => setEditedContent(e.target.value)}
+					/>
+				) : (
+						<div className={boardStyles.polaroid}>
+							<img
+								src={asset.content}
+								alt='Uploaded content'
+							/>
+						</div>
+				);
+			default:
+				return null;
+		}
+	};
 
-  const renderButtons = () => {
-    return isEditing ? (
-      <div className="edit-mode-buttons">
-        <button onClick={handleSaveEdit}>Save</button>
-        <button onClick={() => setIsEditing(false)}>Cancel</button>
-        <button onClick={() => handleDeleteAsset(asset._id)}>Delete</button>
-      </div>
-    ) : (
-      <button onClick={() => setIsEditing(true)}>Edit</button>
-    );
-  };
+	const renderButtons = () => {
+		return isEditing ? (
+			<div className='edit-mode-buttons'>
+				<button onClick={handleSaveEdit}>Save</button>
+				<button onClick={() => setIsEditing(false)}>Cancel</button>
+				<button onClick={() => handleDeleteAsset(asset._id)}>
+					Delete
+				</button>
+			</div>
+		) : (
+			<button onClick={() => setIsEditing(true)}>Edit</button>
+		);
+	};
 
-  return (
-    <div>
-      {renderContent()}
-      {renderButtons()}
-    </div>
-  );
+	return (
+		<div>
+			{renderContent()}
+			{renderButtons()}
+		</div>
+	);
 }
 
 export default MediaItem;
