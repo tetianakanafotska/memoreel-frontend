af4a28180bbe441ddf15480864c86a5dc55439fa
tanyakanf <tanyakanf@gmail.com>
2024-05-29 18:12:30 +0200

    add styling to history page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,15 +1,24 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@context";
-import { Board, MediaItem } from "@components";
+import { MediaItem, Button } from "@components";
 import boardStyles from "../components/styles/Board.module.sass";
 import usersService from "../services/users.service";
 import { X } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
+import historyStyles from "./styles/History.module.sass";
 
 function History() {
   const { user } = useContext(AuthContext);
   const [allBoards, setAllboards] = useState([]);
-  //const [allAssets, setAllAssets] = useState([]);
+
+  const formatDate = (inputDate) => {
+    const date = new Date(inputDate);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() is zero-based
+    const year = date.getFullYear();
+    const formattedDate = `${day}.${month}.${year}`;
+    return formattedDate;
+  };
 
   useEffect(() => {
     if (user) {
@@ -26,18 +35,21 @@ function History() {
   }, [user]);
 
   return (
-    <>
-      <div className="closeBtn">
-        <Link to="/dashboard">{<X size="40" />}</Link>
+    <div className={historyStyles.history}>
+      <div className={historyStyles.dashboardBtn}>
+        <Button to={"/dashboard"}>Dashboard</Button>
       </div>
+
       {allBoards &&
         allBoards.length >= 0 &&
         (allBoards.length === 0
           ? "No board to show yet!"
           : allBoards.reverse().map((board) => {
               return (
-                <div key={board._id}>
-                  <h3>Board {board.createdAt}</h3>
+                <div key={board._id} className={historyStyles.reel}>
+                  <h2 className={historyStyles.date}>
+                    {formatDate(board.createdAt)}
+                  </h2>
                   <div className={boardStyles.board}>
                     {board.assets.length > 0 &&
                       board.assets.reverse().map((asset) => (
@@ -49,7 +61,7 @@ function History() {
                 </div>
               );
             }))}
-    </>
+    </div>
   );
 }
 
