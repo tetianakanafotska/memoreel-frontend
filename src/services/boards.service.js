3271c52336aeea18c0c51720b96068270648ac0c
Laura Sinclair <l.sinclair@tuta.io>
2024-05-29 11:08:31 +0200

    .env changes

--- src/services/boards.service.js
+++ src/services/boards.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class BoardsService {
   constructor() {
     this.api = axios.create({
-      baseURL: import.meta.env.SERVER_URL || "http://localhost:5005",
+      baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005",
     });
 
     this.api.interceptors.request.use((config) => {
